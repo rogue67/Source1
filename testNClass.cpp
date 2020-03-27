ec250c88527c759a7aacdb4d536c8c3ef3c2cc26
rogue67 <rofo6455@cronstedt.it.uu.se>
2020-03-27 21:30:33 +0100

    made another change

---
+++ testNClass.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int nummer;
+int main(){
+    cout << "testfil 1:  " << endl;
+    cout << "Ange nummer:  ";
+    cin >> nummer;
+    cout << "Plus 2: " << nummer + 2 << endl;
+    return 0;
+}
