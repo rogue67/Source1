f566aac7ce5dd3ee3815d0d28467d264aadc3bf7
rogue67 <roger.forsman67@gmail.com>
2020-09-01 22:38:02 +0200

    Test av Personclass och PersonLista

--- testNClass.cpp
+++ testNClass.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+class Person
+{
+private:
+    string namn;
+    double betalat_andras; // ligger ute med totalt
+    double skyldig;        // skyldig totalt
+
+public:
+    Person();
+    Person(string n, double b, double s);
+    string haemta_namn();
+    double haemta_betalat();
+    double haemta_skyldig();
+    void skrivUt();
+};
+
+class PersonLista
+{
+private:
+    int antal_pers;
+    Person *pers;
+
+public:
+    PersonLista();
+    ~PersonLista();
+    void laggTillEn(Person pny);
+    void skrivUtOchFixa();
+    double summaSkyldig();
+    double summaBetalat();
+    bool finnsPerson(const string &namn);
+};
 
 int nummer;
 int main(){
@@ -9,4 +42,90 @@ int main(){
     cin >> nummer;
     cout << "Plus 2: " << nummer + 2 << endl;
     return 0;
+}
+
+Person::Person(){};
+Person::Person(string n, double b, double s)
+    : namn(n), betalat_andras(b), skyldig(s){};
+
+string Person::haemta_namn()
+{
+    return namn;
+}
+
+double Person::haemta_betalat()
+{
+
+    return betalat_andras;
+}
+
+double Person::haemta_skyldig()
+{
+    return skyldig;
+}
+
+void Person::skrivUt()
+{
+    cout << endl
+         << namn << " Ligger ute med : " << betalat_andras << " och är skyldig: " << skyldig;
+    double diff = betalat_andras - skyldig;
+    if (diff > 0)
+        cout << " Skall ha " << diff << " från potten!" << endl;
+    else
+        cout << " Skall betala " << -diff << " till potten!" << endl;
+}
+
+PersonLista::PersonLista()
+{
+    for (int i = 0; i < MAX_PERSONER; i++)
+        pers[i] = 0;
+}
+PersonLista::~PersonLista(){};
+
+void PersonLista::laggTillEn(Person pny)
+{
+    pers[antal_pers] = &pny;
+    antal_pers++;
+}
+
+void PersonLista::skrivUtOchFixa()
+{
+    int i = 0;
+    while (pers[i] != 0)
+    {
+        pers[i]->skrivUt();
+        i++;
+    }
+
+    cout << endl
+         << "Totalt: utlägg - skulder "
+         << " = " << summaBetalat() - summaSkyldig() << endl
+         << endl;
+}
+
+double PersonLista::summaSkyldig()
+{
+    double skyldig = 0.0;
+    for (int i = 0; i < antal_pers; i++)
+        skyldig += pers[i]->haemta_skyldig();
+    return skyldig;
+}
+
+double PersonLista::summaBetalat()
+{
+    double betalat = 0.0;
+    for (int i = 0; i < antal_pers; i++)
+        betalat += pers[i]->haemta_betalat();
+    return betalat;
+}
+
+bool PersonLista::finnsPerson(const string &namn)
+{
+    bool hittad = false;
+    for (int i = 0; i < antal_pers; i++)
+    {
+        if (pers[i]->haemta_namn() == namn)
+            hittad = true;
+    }
+    return hittad;
 }
